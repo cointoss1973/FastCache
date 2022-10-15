8a691815c6f77a23c104fa9652b834f7017bdaae
Takayuki Kondo <tkondou@gmail.com>
2022-10-15 13:26:16 +0900

    Update test code

--- FastCache.Tests/TestHelper.cs
+++ FastCache.Tests/TestHelper.cs
@@ -1,25 +1,24 @@
 ﻿
-namespace FastCache.Tests
+namespace FastCache.Tests;
+
+internal class TestHelper
 {
-	internal class TestHelper
+	public static async Task RunConcurrently(int numThreads, Action action)
 	{
-		public static async Task RunConcurrently(int numThreads, Action action)
+		var tasks = new Task[numThreads];
+		ManualResetEvent m = new ManualResetEvent(false);
+
+		for (int i = 0; i < numThreads; i++)
 		{
-			var tasks = new Task[numThreads];
-			ManualResetEvent m = new ManualResetEvent(false);
-
-			for (int i = 0; i < numThreads; i++)
+			tasks[i] = Task.Run(() =>
 			{
-				tasks[i] = Task.Run(() =>
-				{
-					m.WaitOne(); //dont start just yet
-					action();
-				});
-			}
-
-			m.Set(); //off we go
-
-			await Task.WhenAll(tasks);
+				m.WaitOne(); //dont start just yet
+				action();
+			});
 		}
+
+		m.Set(); //off we go
+
+		await Task.WhenAll(tasks);
 	}
 }
--- FastCache.Tests/UnitTests.cs
+++ FastCache.Tests/UnitTests.cs
@@ -1,189 +1,187 @@
 ﻿using Jitbit.Utils;
 using Xunit;
 
-namespace FastCache.Tests
+namespace FastCache.Tests;
+
+public class UnitTests
 {
-    public class UnitTests
+    [Fact]
+    public async Task TestGetSetCleanup()
     {
-        [Fact]
-        public async Task TestGetSetCleanup()
+        // Arrange
+        var _cache = new FastCache<int, int>(cleanupJobInterval: 200);
+
+        // Act
+        _cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(100));
+
+        // Assert
+        Assert.True(_cache.TryGet(42, out int v));
+        Assert.Equal(42, v);
+
+        await Task.Delay(300);
+        Assert.True(_cache.Count == 0); //cleanup job has run?
+    }
+
+    [Fact]
+    public async Task Shortdelay()
+    {
+        // Arrange
+        var cache = new FastCache<int, int>();
+
+        // Act
+        cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(500));
+        await Task.Delay(50);
+
+        // Assert
+        Assert.True(cache.TryGet(42, out int result)); //not evicted
+        Assert.Equal(42, result);
+    }
+
+    [Fact]
+    public async Task TestWithDefaultJobInterval()
+    {
+        // Arrange
+        var cache = new FastCache<string, int>();
+
+        // Act
+        cache.AddOrUpdate("key", 42, TimeSpan.FromMilliseconds(100));
+
+        // Assert
+        Assert.True(cache.TryGet("key", out _));
+        await Task.Delay(150);
+        Assert.False(cache.TryGet("key", out _));
+    }
+
+    [Fact]
+    public void TestRemove()
+    {
+        // Arrange
+        var cache = new FastCache<string, int>();
+
+        // Act
+        cache.AddOrUpdate("key", 42, TimeSpan.FromMilliseconds(100));
+        cache.Remove("key");
+        bool result = cache.TryGet("key", out _);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public async Task TestTryAdd()
+    {
+        // Arrange
+        var cache = new FastCache<string, int>();
+
+        // Act, Assert
+        Assert.True(cache.TryAdd("key", 42, TimeSpan.FromMilliseconds(100)));
+        Assert.False(cache.TryAdd("key", 42, TimeSpan.FromMilliseconds(100)));
+
+        await Task.Delay(120); //wait for it to expire
+
+        Assert.True(cache.TryAdd("key", 42, TimeSpan.FromMilliseconds(100)));
+    }
+
+    [Fact]
+    public async Task TestGetOrAdd()
+    {
+        // Arrange
+        var cache = new FastCache<string, int>();
+
+        // Act
+        cache.GetOrAdd("key", k => 1024, TimeSpan.FromMilliseconds(100));
+
+        // Assert
+        Assert.True(cache.TryGet("key", out int value));
+        Assert.Equal(1024, value);
+        await Task.Delay(110);
+
+        Assert.False(cache.TryGet("key", out _));
+    }
+
+    [Fact]
+    public async Task TestTryAddAtomicness()
+    {
+        // Arrange
+        int i = 0;
+
+        var cache = new FastCache<int, int>();
+        cache.TryAdd(42, 42, TimeSpan.FromMilliseconds(50)); //add item with short TTL
+
+        // Act
+        await Task.Delay(100); //wait for tha value to expire
+
+        await TestHelper.RunConcurrently(20, () =>
         {
-            // Arrange
-            var _cache = new FastCache<int, int>(cleanupJobInterval: 200);
-
-            // Act
-            _cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(100));
-
-            // Assert
-            Assert.True(_cache.TryGet(42, out int v));
-            Assert.Equal(42, v);
-
-            await Task.Delay(300);
-            Assert.True(_cache.Count == 0); //cleanup job has run?
-        }
-
-        [Fact]
-        public async Task Shortdelay()
-        {
-            // Arrange
-            var cache = new FastCache<int, int>();
-
-            // Act
-            cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(500));
-            await Task.Delay(50);
-
-            // Assert
-            Assert.True(cache.TryGet(42, out int result)); //not evicted
-            Assert.Equal(42, result);
-        }
-
-        [Fact]
-        public async Task TestWithDefaultJobInterval()
-        {
-            // Arrange
-            var cache = new FastCache<string, int>();
-
-            // Act
-            cache.AddOrUpdate("42", 42, TimeSpan.FromMilliseconds(100));
-
-            // Assert
-            Assert.True(cache.TryGet("42", out _));
-            await Task.Delay(150);
-            Assert.False(cache.TryGet("42", out _));
-        }
-
-        [Fact]
-        public void TestRemove()
-        {
-            // Arrange
-            var cache = new FastCache<string, int>();
-
-            // Act
-            cache.AddOrUpdate("key", 42, TimeSpan.FromMilliseconds(100));
-            cache.Remove("key");
-            bool result = cache.TryGet("key", out _);
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public async Task TestTryAdd()
-        {
-            // Arrange
-            var cache = new FastCache<string, int>();
-
-            // Act, Assert
-            Assert.True(cache.TryAdd("key", 42, TimeSpan.FromMilliseconds(100)));
-            Assert.False(cache.TryAdd("key", 42, TimeSpan.FromMilliseconds(100)));
-
-            await Task.Delay(120); //wait for it to expire
-
-            Assert.True(cache.TryAdd("key", 42, TimeSpan.FromMilliseconds(100)));
-        }
-
-        [Fact]
-        public async Task TestGetOrAdd()
-        {
-            // Arrange
-            var cache = new FastCache<string, int>();
-
-            // Act
-            cache.GetOrAdd("key", k => 1024, TimeSpan.FromMilliseconds(100));
-
-            // Assert
-            Assert.True(cache.TryGet("key", out int res) && res == 1024);
-            await Task.Delay(110);
-
-            Assert.False(cache.TryGet("key", out _));
-        }
-
-        [Fact]
-        public async Task TestTryAddAtomicness()
-        {
-            // Arrange
-            int i = 0;
-
-            // Act
-            var cache = new FastCache<int, int>();
-            cache.TryAdd(42, 42, TimeSpan.FromMilliseconds(50)); //add item with short TTL
-
-            await Task.Delay(100); //wait for tha value to expire
-
-            await TestHelper.RunConcurrently(20, () =>
-            {
-                if (cache.TryAdd(42, 42, TimeSpan.FromSeconds(1)))
-                    i++;
-            });
-
-            // Assert
-            Assert.True(i == 1, i.ToString());
-        }
-
-        [Fact]
-        public async Task TestGetOrAddAtomicNess()
-        {
-            // Arrange
-            int i = 0;
-
-            var cache = new FastCache<int, int>();
-
-            // Act
-            cache.GetOrAdd(42, k =>
-            {
+            if (cache.TryAdd(42, 42, TimeSpan.FromSeconds(1)))
                 i++;
-                return 1024;
-            }, TimeSpan.FromMilliseconds(100));
+        });
 
+        // Assert
+        Assert.True(i == 1, i.ToString());
+    }
 
-            await TestHelper.RunConcurrently(20, () =>
-            {
-                if (cache.TryAdd(42, 42, TimeSpan.FromSeconds(1)))
-                    i++;
-            });
+    [Fact]
+    public async Task TestGetOrAddAtomicNess()
+    {
+        // Arrange
+        int i = 0;
 
-            // Assert
-            // test that add factory was called only once
-            Assert.True(i == 1, i.ToString());
-        }
-
-        [Fact]
-        public async Task Enumerator()
+        var cache = new FastCache<int, int>();
+        cache.GetOrAdd(42, k =>
         {
-            // Arrange
-            var cache = new FastCache<string, int>(); //now with default cleanup interval
-            cache.GetOrAdd("key", k => 1024, TimeSpan.FromMilliseconds(100));
+            i++;
+            return 1024;
+        }, TimeSpan.FromMilliseconds(100));
 
-            // Act
-            int value = cache.FirstOrDefault().Value;
-
-            // Assert
-            Assert.Equal(1024, value);
-
-            await Task.Delay(110);
-
-            Assert.False(cache.Any());
-        }
-
-        [Fact]
-        public async Task TestTtlExtended()
+        // Act
+        await TestHelper.RunConcurrently(20, () =>
         {
-            // Arrange
-            var _cache = new FastCache<int, int>();
-            _cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(300));
+            if (cache.TryAdd(42, 42, TimeSpan.FromSeconds(1)))
+                i++;
+        });
 
-            // Act
-            await Task.Delay(50);
-            Assert.True(_cache.TryGet(42, out int result)); //not evicted
-            Assert.True(result == 42);
+        // Assert
+        // test that add factory was called only once
+        Assert.True(i == 1, i.ToString());
+    }
 
-            _cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(300));
+    [Fact]
+    public async Task Enumerator()
+    {
+        // Arrange
+        var cache = new FastCache<string, int>(); //now with default cleanup interval
+        cache.GetOrAdd("key", k => 1024, TimeSpan.FromMilliseconds(100));
 
-            await Task.Delay(250);
+        // Act
+        int value = cache.FirstOrDefault().Value;
 
-            // Assert
-            Assert.True(_cache.TryGet(42, out int result2)); //still not evicted
-            Assert.True(result2 == 42);
-        }
+        // Assert
+        Assert.Equal(1024, value);
+
+        await Task.Delay(110);
+
+        Assert.False(cache.Any());
+    }
+
+    [Fact]
+    public async Task TestTtlExtended()
+    {
+        // Arrange
+        var _cache = new FastCache<int, int>();
+
+        // Act
+        _cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(300));
+        await Task.Delay(50);
+        Assert.True(_cache.TryGet(42, out int result)); //not evicted
+        Assert.True(result == 42);
+
+        _cache.AddOrUpdate(42, 42, TimeSpan.FromMilliseconds(300));
+
+        await Task.Delay(250);
+
+        // Assert
+        Assert.True(_cache.TryGet(42, out int result2)); //still not evicted
+        Assert.True(result2 == 42);
     }
 }
